3e1f8e505b6ef5dc27723d9f9ecdce923ac87a25
Anton Venema <anton.venema@frozenmountain.com>
2020-10-15 14:26:54 -0700

    DRY

--- src/FM.Resp/OutputVerb.cs
+++ src/FM.Resp/OutputVerb.cs
@@ -1,4 +1,11 @@
-﻿namespace FM.Resp
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FM.Resp
 {
     abstract class OutputVerb<TOptions> : InputVerb<TOptions>
         where TOptions : OutputOptions
@@ -6,5 +13,37 @@
         protected OutputVerb(TOptions options)
             : base(options)
         { }
+
+        protected override async Task<int> ProcessStream(Stream stream)
+        {
+            var elements = await ProcessStreamIntoElements(stream).ConfigureAwait(false);
+
+            Console.Error.Write("Serializing to JSON...");
+            var json = JsonConvert.SerializeObject(elements, Options.Indented ? Formatting.Indented : Formatting.None, new JsonSerializerSettings
+            {
+                NullValueHandling = NullValueHandling.Ignore
+            });
+            Console.Error.WriteLine(" done.");
+
+            foreach (var type in new[] { DataType.SimpleString, DataType.Error, DataType.Integer, DataType.BulkString, DataType.Array })
+            {
+                Console.Error.WriteLine($"{type} count: {elements.Count(x => x.Type == type)}");
+            }
+
+            if (Options.Output != null)
+            {
+                Console.Error.Write("Writing to output file...");
+                await File.WriteAllTextAsync(Options.Output, json);
+                Console.Error.WriteLine(" done.");
+            }
+            else
+            {
+                Console.WriteLine(json);
+            }
+
+            return 0;
+        }
+
+        protected abstract Task<IEnumerable<Element>> ProcessStreamIntoElements(Stream stream);
     }
 }
